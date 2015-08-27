036525b38ac3080f7068ee8c98bab641af5a4d7e
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-08-27 05:57:32 +0200

    Initial Valid Solution

---
+++ Euler5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Euler5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n;
+            uint.TryParse(args[0], out n);
+
+            ulong p = 2;
+
+            for (uint i = 3; i <= n; i++)
+            {
+                p = p * i / GCD(p, i);
+            }
+
+            Console.WriteLine(p);
+            Console.ReadLine();
+        }
+
+        static ulong GCD(ulong a, ulong b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+    }
+}
